9610c8853991f75a4e24d7f09e99334fba42df10
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2023-09-14 11:00:15 +0200

    Add files via upload

---
+++ creer_des_resources_PLUS.sh
@@ -0,0 +1,22 @@
+
+
+  
+  <form action="/action_page.php">
+      <label for="fname">TEXT:</label>
+      <input type="text" id="fname" name="fname"><br><br>
+      <input type="submit" value="Submit" onclick="send_data()">
+  </form>
+
+
+
+# C'est signifie que le fichier a besoin d'etre sur Azure 
+azcopy login
+azcopy copy 'C:\myDirectory\myFile.txt' 'https://mystorageaccount.blob.core.windows.net/mycontainer/myFile.txt'
+
+https://docs.microsoft.com/en-US/cli/azure/storage/account#az_storage_account_show
+
+
+
+
+
+
